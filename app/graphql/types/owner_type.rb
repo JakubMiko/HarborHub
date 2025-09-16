76cf755c1d1f23533ca0f1ac0e7c482a8eaf09a3
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-09-16 18:32:12 +0200

    create full_name method

--- app/graphql/types/owner_type.rb
+++ app/graphql/types/owner_type.rb
@@ -9,5 +9,12 @@ module Types
     field :last_name, String, null: false
     field :yob, Integer, null: false
     field :is_verified, Boolean, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :full_name, String, null: true
+
+    def full_name
+      "#{object.first_name} #{object.last_name}"
+    end
   end
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,6 @@
 class Owner < ApplicationRecord
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
